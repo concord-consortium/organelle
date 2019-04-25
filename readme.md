c162378aa22edbd707d3b1f5de4d2e255afbf23d
Kirk Swenson <kswenson@concord.org>
2019-04-25 09:34:48 -0700

    Configure Webpack for debugging with webpack-dev-server Add Geniventure challenge test links to default page

--- readme.md
+++ readme.md
@@ -3,11 +3,7 @@
 An agent-based modeling library based on SVG and declarative rules.
 
 ## Developer setup
-
-    yarn
-    npm start
-
-    // view the contents of /dist in your browser.
-    // e.g. using live-server, in another tab:
-    npm install -g live-server
-    live-server dist
+```
+npm install   # install dependencies
+npm start     # start dev-server; open browser to localhost:8080
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,46 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    'app': './src/app.js',
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: "organelle.js",
-    library: 'Organelle'
-  },
-  performance: { hints: false },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = function(env, argv) {
+  const isDevMode = argv.mode !== 'production';
+  return {
+    entry: {
+      'app': './src/app.js',
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "organelle.js",
+      library: 'Organelle',
+      libraryExport: 'default'
+    },
+    devtool: isDevMode ? 'eval-source-map' : 'source-map',
+    devServer: {
+      contentBase: './dist'
+    },
+    performance: { hints: false },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
+        },
+        {
+          // See https://github.com/adobe-webplatform/Snap.svg/issues/341
+          test: require.resolve('snapsvg'),
+          loader: 'imports-loader?this=>window,fix=>module.exports=0'
         }
-      },
-      {
-        // See https://github.com/adobe-webplatform/Snap.svg/issues/341
-        test: require.resolve('snapsvg'),
-        loader: 'imports-loader?this=>window,fix=>module.exports=0'
-      }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {from: __dirname + '/public'},
+        {from: __dirname + '/models', to: 'models'}
+      ])
     ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {from: __dirname + '/public'},
-      {from: __dirname + '/models', to: 'models'}
-    ])
-  ]
+  }
 };
